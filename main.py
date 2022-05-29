8b34bae388f19c2511f53fcb0b5da26f188046cc
Pedro Alves <pedron18@uia.no>
2022-05-29 14:48:47 +0200

    small bugifx adding to readme

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ if train:
 			# Update state
 			observation = next_observation
 
-			if done or time.time() - startTime > 500:
+			if done or time.time() - start > 500:
 				break
 
 		logger.log_episode()
